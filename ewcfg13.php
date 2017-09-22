98f9c51ae0217c85ae4b2768a3d30f421f7b851d
olapps3998 probook <olapps3998@gmail.com>
2017-09-22 21:17:51 +0700

    1

--- ewcfg13.php
+++ ewcfg13.php
@@ -25,7 +25,7 @@ define("EW_CONFIG_FILE_FOLDER", EW_PROJECT_NAME, TRUE); // Config file name
 define("EW_PROJECT_ID", "{939D1C58-B1B5-41D0-A0B9-205FEFFF0852}", TRUE); // Project ID (GUID)
 $EW_RELATED_PROJECT_ID = "";
 $EW_RELATED_LANGUAGE_FOLDER = "";
-define("EW_RANDOM_KEY", '5ye5HFy47tvbJHaX', TRUE); // Random key for encryption
+define("EW_RANDOM_KEY", '6vfHrt54R0v7b2cP', TRUE); // Random key for encryption
 define("EW_PROJECT_STYLESHEET_FILENAME", "phpcss/prj_stok.css", TRUE); // Project stylesheet file name
 define("EW_CHARSET", "", TRUE); // Project charset
 define("EW_EMAIL_CHARSET", EW_CHARSET, TRUE); // Email charset
