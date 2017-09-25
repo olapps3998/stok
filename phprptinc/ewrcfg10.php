edabdbcf65e7588e8d3e9909a68cb9dfde2b0d81
olapps3998 probook <olapps3998@gmail.com>
2017-09-25 12:18:05 +0700

    1

--- phprptinc/ewrcfg10.php
+++ phprptinc/ewrcfg10.php
@@ -79,7 +79,7 @@ define("EWR_SESSION_TIMEOUT_COUNTDOWN", 60, TRUE); // Session timeout count down
 // General
 $EWR_ROOT_RELATIVE_PATH = "."; // Relative path of app root
 define("EWR_UNFORMAT_YEAR", 50, TRUE); // Unformat year
-define("EWR_RANDOM_KEY", 'TNq5qsE7jquZpTdg', TRUE); // Random key for encryption
+define("EWR_RANDOM_KEY", 'lNqO0X6Nr5bAeFKG', TRUE); // Random key for encryption
 define("EWR_PROJECT_STYLESHEET_FILENAME", "phprptcss/prj_stok.css", TRUE); // Project stylesheet file name
 define("EWR_CHART_WIDTH", 550, TRUE);
 define("EWR_CHART_HEIGHT", 440, TRUE);
